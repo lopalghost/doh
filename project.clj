5c7a119927e4de1f03658e7d6e876694ddfbc12f
Gary Nalven <gnalven@gmail.com>
2016-09-16 11:32:57 -0400

    updated project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/lopalghost/doh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]]
+  :deploy-repositories {"clojars" {:url "https://clojars.org/repo"
+                                   :sign-releases false}})
