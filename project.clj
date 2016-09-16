f919f333b5ce740b1b0921cbb1c6c72f93a61671
lopalghost <gnalven@gmail.com>
2016-09-16 10:23:34 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject doh "0.1.0-alpha"
   :description "Simple error hanlding for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/lopalghost/doh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha12"]])
